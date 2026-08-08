800eb2cd3c3fee6c2a5d8c7dc0f8fbb9697798d1
ZCode <zcode@local>
2026-08-08 19:31:56 +0800

    chore: package.sh 版本号更新至 0.7.3 + Rust core 重新编译

--- scripts/package.sh
+++ scripts/package.sh
@@ -26,8 +26,8 @@ SCHEME_NAME="FlowFinderNative"
 TARGET_NAME="FlowFinderNative"
 CONFIGURATION="Release"
 BUILD_DIR="$PROJECT_DIR/build"
-VERSION="0.6.4"
-BUILD_NUMBER="640"
+VERSION="0.7.3"
+BUILD_NUMBER="730"
 
 # Optional Developer ID signing (set via environment)
 DEVELOPER_ID="${DEVELOPER_ID:-}"
